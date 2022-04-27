fc109eb4064fbed1be8eef99a302556f2212dd24
nguyentranngoctrong <trongb1906792@student.ctu.edu.vn>
2022-04-27 21:13:57 +0700

    Cai dat

---
+++ routes/api/posts.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+//Get Posts
+router.get('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    res.send(await posts.find({}).toArray());
+});
+
+// Add Post
+router.post('/', async (req, res) =>{
+    const posts = await loadPostsCollection();
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    });
+    res.status(201).send();
+})
+// Delete Post
+router.delete('/:id', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.deleteOne({_id: new mongodb.ObjectId(req.params.id)});
+    res.status(200).send();
+});
+
+async function loadPostsCollection() {
+    const client = await mongodb.MongoClient.connect (
+        'mongodb://localhost:27017/miniweb', {
+            useNewUrlParser: true
+        })
+    return client.db('miniweb').collection('posts');
+}
+
+module.exports = router;
